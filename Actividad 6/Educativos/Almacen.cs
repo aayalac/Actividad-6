e4ac8d04c65398c59172a9cacc3b309717f11c60
aayalac <aayalac@misena.edu.co>
2022-09-02 10:11:58 -0500

    Agregar archivos de proyecto.

---
+++ Actividad 6/Educativos/Almacen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_6.Educativos
+{
+    internal class Almacen
+    {
+    }
+}
